7b198f80241b41b3b747969f9125047dd1f4406a
Carlos Sousa <103946120+kcarww@users.noreply.github.com>
2023-09-08 19:11:49 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -1,112 +1,130 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
 from tkinter import ttk
 
+agenda = []
 
-def add_contact():
-    name = name_entry.get()
-    phone = phone_entry.get()
-    category = category_combobox.get()
-    new_contact = {"Nome": name, "Telefone": phone, "Categoria": category}
-    contacts.append(new_contact)
-    update_treeview()
-    clear_fields()
-
-
-def update_treeview():
-    for row in tree.get_children():
-        tree.delete(row)
-
-    query = search_entry.get().lower()
-    for contact in contacts:
-        if query in contact['Nome'].lower():
-            tree.insert("", tk.END, values=(contact["Nome"], contact["Telefone"], contact["Categoria"]))
+def adicionarContato() -> None:
+    # Pegar os valores digitados
+    nome = txt_nome.get()
+    telefone = txt_telefone.get()
+    categoria = cb_categoria.get()
+    contato = {
+        "nome": nome,
+        "telefone": telefone,
+        "categoria": categoria
+    }
+    agenda.append(contato)
+    messagebox.showinfo("Adicionado!", "Contato adicionado com sucesso!")
+    limparCampos()
+    atualizarTabela()
 
 
-def clear_fields():
-    name_entry.delete(0, tk.END)
-    phone_entry.delete(0, tk.END)
-    category_combobox.set("")
-
-
-def edit_contact():
-    selected_items = tree.selection()
-    if not selected_items:
+def editarContato() -> None:
+    contato_selecionado = tabela.selection()[0]
+    if not contato_selecionado:
         return
-    selected_item = selected_items[0]
-    index = tree.index(selected_item)
-    contacts[index] = {"Nome": name_entry.get(), "Telefone": phone_entry.get(), "Categoria": category_combobox.get()}
-    update_treeview()
-    clear_fields()
+    index = tabela.index(contato_selecionado)
+    agenda[index] = {
+        "nome": txt_nome.get(),
+        "telefone": txt_telefone.get(),
+        "categoria": cb_categoria.get()
+    }
+    atualizarTabela()
+    limparCampos()
 
 
-def delete_contact():
-    selected_items = tree.selection()
-    if not selected_items:
+def deletarContato() -> None:
+    contato_selecionado = tabela.selection()[0]
+    if not contato_selecionado:
         return
-    selected_item = selected_items[0]
-    index = tree.index(selected_item)
-    del contacts[index]
-    update_treeview()
-    clear_fields()
+    index = tabela.index(contato_selecionado)
+    del agenda[index]
+    limparCampos()
+    atualizarTabela()
 
 
-def on_tree_select(event):
-    selected_items = tree.selection()
-    if not selected_items:
+def limparCampos() -> None:
+    txt_nome.delete(0, END)
+    txt_telefone.delete(0, END)
+    cb_categoria.delete(0, END)
+
+def atualizarTabela() -> None:
+    # Limpando a tabela
+    # get_children -> retorna uma lista com as linhas da tabela
+    for linha in tabela.get_children():
+        tabela.delete(linha)
+
+    for contato in agenda:
+        tabela.insert("", END, values=(contato["nome"], contato["telefone"],
+                      contato["categoria"]))
+
+
+def tabelaClique(event) -> None:
+    contato_selecionado = tabela.selection()[0]
+    if not contato_selecionado:
         return
-    selected_item = selected_items[0]
-    index = tree.index(selected_item)
-    contact = contacts[index]
-    name_entry.delete(0, tk.END)
-    name_entry.insert(0, contact["Nome"])
-    phone_entry.delete(0, tk.END)
-    phone_entry.insert(0, contact["Telefone"])
-    category_combobox.set(contact["Categoria"])
+    index = tabela.index(contato_selecionado)
+    # preenchendo os campos com o contato do index da tabela
+    contato = agenda[index]
+    limparCampos()
+    txt_nome.insert(0, contato["nome"])
+    txt_telefone.insert(0, contato["telefone"])
+    cb_categoria.set(contato["categoria"])
 
 
-def on_search(*args):
-    update_treeview()
+
+janela = Tk()
+# Como adicionar titulo na janela
+janela.title("Agenda telefonica")
+
+label_nome = Label(janela, text="Nome: ", fg="navy", font="Tahoma 14 bold")
+label_nome.grid(row=0, column=0)
+
+txt_nome = Entry(janela, font="Tahoma 14", width=27)
+txt_nome.grid(row=0, column=1)
+
+label_telefone = Label(janela, text="Telefone: ", fg="navy", font="Tahoma 14 bold")
+label_telefone.grid(row=1, column=0)
+
+txt_telefone = Entry(janela, font="Tahoma 14", width=27)
+txt_telefone.grid(row=1, column=1)
+
+label_categoria = Label(janela, text="Categoria: ", fg="navy", font="Tahoma 14 bold")
+label_categoria.grid(row=2, column=0)
+categorias = ["Amigos", "Familia", "Trabalho"]
+cb_categoria = ttk.Combobox(janela, values=categorias, width=25,
+                            font="Tahoma 14")
+cb_categoria.grid(row=2, column=1)
 
 
-contacts = []
-root = tk.Tk()
-root.title("Agenda de Contatos")
 
-name_label = tk.Label(root, text="Nome")
-name_label.grid(row=0, column=0)
-name_entry = tk.Entry(root, width=25)
-name_entry.grid(row=0, column=1)
+btn_adicionar = Button(janela, text="Adicionar", fg="navy",
+                       bg="white", font="Tahoma 12 bold", width=8, height=1,
+                       command=adicionarContato)
+btn_adicionar.grid(row=3, column=0)
 
-phone_label = tk.Label(root, text="Telefone")
-phone_label.grid(row=1, column=0)
-phone_entry = tk.Entry(root, width=25)
-phone_entry.grid(row=1, column=1)
+btn_editar = Button(janela, text="Editar", fg="navy",
+                    bg="white", font="Tahoma 12 bold", width=8, height=1,
+                    command=editarContato)
+btn_editar.grid(row=3, column=1)
 
-category_label = tk.Label(root, text="Categoria")
-category_label.grid(row=2, column=0)
-category_combobox = ttk.Combobox(root, values=["Trabalho", "Família", "Amigos"], width=23)
-category_combobox.grid(row=2, column=1)
+btn_excluir = Button(janela, text="Excluir", fg="navy",
+                     bg="white", font="Tahoma 12 bold", width=8, height=1,
+                     command=deletarContato)
+btn_excluir.grid(row=3, column=2)
 
-add_button = tk.Button(root, text="Adicionar", command=add_contact)
-add_button.grid(row=3, column=0)
 
-edit_button = tk.Button(root, text="Editar", command=edit_contact)
-edit_button.grid(row=3, column=1)
+# Como criar uma tabela => TreeView
+tabela = ttk.Treeview(janela, columns=("Nome", "Telefone", "Categoria"),
+                      show="headings")
+tabela.heading("Nome", text="Nome")
+tabela.heading("Telefone", text="Telefone")
+tabela.heading("Categoria", text="Categoria")
+# Criando a ação/bind para quando o usuário clicar num contato
+# da tabela
+tabela.bind("<ButtonRelease-1>", tabelaClique)
+tabela.grid(row=4, columnspan=3)
 
-delete_button = tk.Button(root, text="Excluir", command=delete_contact)
-delete_button.grid(row=3, column=2)
 
-tree = ttk.Treeview(root, columns=("Nome", "Telefone", "Categoria"), show="headings")
-tree.heading("Nome", text="Nome")
-tree.heading("Telefone", text="Telefone")
-tree.heading("Categoria", text="Categoria")
-tree.grid(row=4, columnspan=3)
-tree.bind("<<TreeviewSelect>>", on_tree_select)
-
-search_label = tk.Label(root, text="Pesquisar")
-search_label.grid(row=5, column=0)
-search_entry = tk.Entry(root, width=50)
-search_entry.grid(row=5, column=1)
-search_entry.bind("<KeyRelease>", on_search)
-
-root.mainloop()
+janela.mainloop()
